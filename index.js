bd4930983ec6b04e46d6f88fd6b812548366e098
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 18:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function findMatching(list, name) {
   });
 }
 
-function fuzzyMatch(list, name) {
+function fuzzyMatch(list, partialName) {
   let lengthOfName = partialName.length;
   return list.filter(function (driverName) {
     return driverName.slice(0, lengthOfName) === partialName;
